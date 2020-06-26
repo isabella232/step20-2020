c0a3d55f2f623677d9c11a7e191bce2258629f22
Tommy Gomez <tomgom@google.com>
2020-06-26 01:24:04 +0000

    add button to make GET request to servlet for simulating making spin off recipes

---
+++ src/main/webapp/script.js
@@ -0,0 +1,21 @@
+function getOriginalRecipe() {
+  const key = document.getElementById("key").value;
+  if (key !== "") {
+    fetch("/new-recipe?key=" + key).then(response => response.json()).then((recipe) => {
+      console.log(recipe);
+      populateRecipeCreationForm(recipe);
+    });
+  }
+}
+
+function populateRecipeCreationForm(recipe) {
+  var name = document.getElementById("name");
+  name.value = recipe.name;
+
+  var description = document.getElementById("description");
+  description.value = recipe.name;
+
+  populateFormComponent("tag", recipe.tags);
+  populateFormComponent("ingredient", recipe.ingredients);
+  populateFormComponent("step", recipe.steps);
+}
