86b64a7ffc694806118ed0b33b7df08d14af9084
Tommy Gomez <tomgom@google.com>
2020-06-30 00:44:08 +0000

    address reviews in Recipe.java

--- src/main/java/data/Recipe.java
+++ src/main/java/data/Recipe.java
@@ -14,21 +14,34 @@
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.*;
+import java.util.Iterator;
 
+/**
+ * Stores a recipe's data.
+ */
 public class Recipe {
+
+  private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
   protected String name;
   protected String description;
   protected List<Step> steps;
   protected List<SpinOff> spinOffs;
 
-  public Recipe(Recipe original) {
-    this.name = original.name;
-    this.description = original.description;
-    this.steps = original.steps;
-    this.spinOffs = original.spinOffs;
+  /**
+   * Copy constructor called when creating spin-offs.
+   */
+  public Recipe(Recipe recipe) {
+    this.name = recipe.name;
+    this.description = recipe.description;
+    this.steps = recipe.steps;
+    this.spinOffs = recipe.spinOffs;
   }
   
+  /**
+   * Default constructor called when creating a new recipe.
+   */
   public Recipe(String name, String description, List<Step> steps) {
     this.name = name;
     this.description = description;
@@ -36,80 +49,114 @@ public class Recipe {
     this.spinOffs = new LinkedList<>();
   }
 
+  /**
+   * Appends a new step to a recipe's list of steps.
+   */
   public void appendStep(Step newStep) {
     steps.add(newStep);
   }
 
-  public void addStep(int position, Step newStep) {
-    if (position < 0) {
-      System.err.println("Position " + position + " out of bounds, failed to add step [" + newStep + "]");
-    } else if (position >= steps.size()) {
-      System.err.println("Position " + position + " out of bounds, appending step [" + newStep + "]");
+  /**
+   * Adds a step to a specified position in a recipe's list of steps.
+   * @param position The position at which to insert. 
+   * @param newStep The new step to insert.
+   * @throws IndexOutOfBoundsException Thrown if position is out of bounds of the recipe's list of steps.
+   */
+  public void addStep(int position, Step newStep) throws IndexOutOfBoundsException {
+    if (position == steps.size()) {
       appendStep(newStep);
+    } else if (!isValidStepPosition(position)) {
+      handleStepException("Position " + position + "is out of bounds [0, " + (steps.size() - 1) + "]. " +
+          "Failed to add step " + newStep + ".");
     } else {
       steps.add(position, newStep);
     }
   }
 
-  public void setStep(int position, Step newStep) {
+  /**
+   * Sets the step at the specified position in a recipe's list of steps.
+   * @param position The position of the step to set. 
+   * @param newStep The replacing step.
+   * @throws IndexOutOfBoundsException Thrown if position is out of bounds of the recipe's list of steps.
+   */
+  public void setStep(int position, Step newStep) throws IndexOutOfBoundsException {
     if (!isValidStepPosition(position)) {
+      handleStepException("Position " + position + "is out of bounds [0, " + (steps.size() - 1) + "]. " +
+          "Failed to set step " + newStep + ".");
       return;
     }
     steps.set(position, newStep);
   }
 
-  public void removeStep(int position) {
+  /**
+   * Removes the step at the specified position in a recipe's list of steps.
+   * @param position The position of the step to remove. 
+   * @throws IndexOutOfBoundsException if position is out of bounds of the recipe's list of steps.
+   */
+  public void removeStep(int position) throws IndexOutOfBoundsException {
     if (!isValidStepPosition(position)) {
+      handleStepException("Position " + position + "is out of bounds [0, " + (steps.size() - 1) + "]. " +
+          "Failed to remove step.");
       return;
     }
     steps.remove(position);
   }
 
+  /** Returns the recipe's list of steps. */
   public List<Step> getSteps() {
     return steps;
   }
 
-  protected void addSpinOff(SpinOff spinOff) {
-    spinOffs.add(spinOff);
-  }
-
-  protected boolean isValidStepPosition(int position) {
-    if (position < 0 || position > steps.size() - 1) {
-      return false;
-    }
-    return true;
-  }
-
+  /** Returns the recipe as a string. */
   @Override
   public String toString() {
-    String str = "\nName: ";
-    str += name;
-    str += "\nDescription: ";
-    str += description;
+    String str = String.format("\nName: %s", name);
+    str += String.format("\nDescription: %s", description);
     str += "\nSteps:\n";
     for (Step step : steps) {
-      str += "\t";
-      str += step.getInstruction();
-      str += "\n";
+      str += String.format("%s-20\n", step.getInstruction());
     }
     return str;
   }
 
+  /** Checks if this recipe is equal to another object. */
   @Override
   public boolean equals(Object other) {
     return other instanceof Recipe && equals(this, (Recipe) other);
   }
 
+  /** Adds a spin-off to the recipe's list of spin-offs. */
+  protected void addSpinOff(SpinOff spinOff) {
+    spinOffs.add(spinOff);
+  }
+
+  /**
+   * Checks if a position is valid within the recipe's list of steps.
+   * @param position The position to check.
+   * @return True if the position is valid, false otherwise.
+   */
+  protected boolean isValidStepPosition(int position) {
+    return position >= 0 && position < steps.size();
+  }
+
+  private void handleStepException(String exceptionText) throws IndexOutOfBoundsException {
+    LOGGER.log(Level.INFO, exceptionText);
+    throw new IndexOutOfBoundsException(exceptionText);
+  }
+
   private static boolean equals(Recipe a, Recipe b) {
-    if (a.steps.size() != b.steps.size()) {
+    if (a.steps.size() != b.steps.size() || !a.name.equals(b.name) || !a.description.equals(b.description)) {
       return false;
     }
-    boolean sameSteps = true;
-    for (int i = 0; i < a.steps.size(); i++) {
-      Step aStep = a.steps.get(i);
-      Step bStep = b.steps.get(i);
-      sameSteps = aStep.getInstruction().equals(bStep.getInstruction());
+    Iterator<Step> aSteps = a.steps.iterator();
+    Iterator<Step> bSteps = b.steps.iterator();
+    while (aSteps.hasNext() && bSteps.hasNext()) {
+      Step aStep = aSteps.next();
+      Step bStep = bSteps.next();
+      if (!aStep.getInstruction().equals(bStep.getInstruction())) {
+        return false;
+      }
     }
-    return sameSteps && a.name.equals(b.name) && a.description.equals(b.description);
+    return true;
   }
 }
