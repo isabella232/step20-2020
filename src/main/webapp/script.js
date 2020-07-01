4c112cfc11581168742a47e307e57b2f9bce5409
Tommy Gomez <tomgom@google.com>
2020-07-01 20:19:16 +0000

    add custom element for ParameterField

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,36 @@
+class ParameterField extends HTMLElement {
+  constructor() {
+    super();
+    const br = document.createElement('br');
+    var wrapper = document.createElement('span');
+    wrapper.class = "parameter-field";
+
+    var name = this.getAttribute('name');
+    var index = this.getAttribute('index');
+    var paramName = name.toLowerCase() + index;
+    
+    var label = document.createElement('label');
+    label.innerText = name + " " + index;
+    label.for = paramName;
+
+    var textArea = document.createElement('textarea');
+    textArea.id = paramName;
+    textArea.name = paramName;
+    textArea.rows = "1";
+
+    var button = document.createElement('button');
+    button.onclick = "addParameterField(" + name + ", " + (index + 1) + ")";
+    button.innerText = "Add " + name;
+
+    wrapper.appendChild(label);
+    wrapper.appendChild(br);
+    wrapper.appendChild(textArea);
+    wrapper.appendChild(button);
+    wrapper.appendChild(br);
+    wrapper.appendChild(br);
+  }
+}
+
 function getOriginalRecipe() {
   const key = document.getElementById("key").value;
   if (key) {
