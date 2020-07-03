af0872ebf41522e90655f88d5c6f19cf9ec5555a
Tommy Gomez <tomgom@google.com>
2020-07-03 19:26:21 +0000

    change parameter-input to use global variables

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,38 @@
+class ParameterField extends HTMLElement {
+  constructor() {
+    super();
+    this.label = document.createElement('label');
+    this.textArea = document.createElement('textarea');
+    this.button = document.createElement('button');
+    this.container = document.createElement('div');
+
+    this.container.appendChild(this.label);
+    this.container.appendChild(this.textArea);
+    this.container.appendChild(this.button);
+  }
+
+  connectedCallback() {
+    this.name = this.getAttribute('name');
+    this.index = parseInt(this.getAttribute('index'));
+    var paramName = this.name.toLowerCase() + this.index;
+
+    this.label.innerText = this.name + " " + (this.index + 1);
+    this.label.for = paramName;
+
+    this.textArea.id = paramName;
+    this.textArea.name = paramName;
+    this.textArea.rows = "1";
+
+    this.button.type = "button";
+    this.button.onclick = event => addParameterInput(this.name, this.index + 1);
+    this.button.innerText = "Add " + this.name;
+
+    this.field = document.getElementById(this.name + 's');
+    this.field.appendChild(this.container);
+  }
+}
+customElements.define('parameter-field', ParameterField);
+
 function getOriginalRecipe() {
   const key = document.getElementById("key").value;
   if (key) {
