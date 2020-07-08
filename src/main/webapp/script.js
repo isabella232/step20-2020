5e724cd4c355587d0b197170d34b6fc22a53a3f4
Tommy Gomez <tomgom@google.com>
2020-07-08 16:44:46 +0000

    fix edit-recipe.js to display step text

---
+++ src/main/webapp/script.js
@@ -0,0 +1,196 @@
+/** @class A custom element that represents a parameter input. */
+class ParameterInput extends HTMLElement {
+
+  /** Creates DOM for a ParameterInput. */
+  constructor() {
+    super();
+    this.label = document.createElement('label');
+    this.textArea = document.createElement('textarea');
+    this.addButton = document.createElement('button');
+    this.deleteButton = document.createElement('button');
+    this.container = document.createElement('div');
+
+    this.container.appendChild(this.label);
+    this.container.appendChild(this.textArea);
+    this.container.appendChild(this.addButton);
+    this.container.appendChild(this.deleteButton);
+  }
+
+  /** Once the attributes for the ParameterInput exist, set its values accordingly. */
+  connectedCallback() {
+    this.name = this.getAttribute('name');
+    this.index = parseInt(this.getAttribute('index'));
+    this.parent = this.name + 's';
+
+    this.textArea.rows = '1';
+    this.addButton.type = 'button';
+    this.addButton.innerText = 'Add ' + this.name;
+    this.deleteButton.type = 'button';
+    this.deleteButton.innerText = 'Delete ' + this.name;
+    this.setIndexAttributes();
+
+    this.appendChild(this.container);
+  }
+
+  /**
+   * Sets all attributes that depend on the ParameterInput's index.
+   * These attributes are separated into their own method so that they can be updated
+   * when ParameterInputs change position.
+   */
+  setIndexAttributes() {
+    var paramName = this.name.toLowerCase() + this.index;
+    this.id = this.name + this.index;
+
+    this.label.innerText = this.name + ' ' + (this.index + 1);
+    this.label.for = paramName;
+
+    this.textArea.name = paramName;
+
+    // Inserts a new ParameterInput below the one clicked.
+    this.addButton.onclick = event => {
+      var newParameter = createParameterInput(this.name, this.index + 1);
+      insertParameterInput(this, newParameter);
+    }
+
+    // Deletes the ParameterInput clicked.
+    this.deleteButton.onclick = event => {
+      const fieldName = this.field;
+      const startIndex = this.index;
+      this.remove();
+      updateIndeces(fieldName, startIndex);
+    }
+  }
+
+  /** Gets the text in a ParameterInput's text area. */
+  get text() {
+    return this.textArea.value;
+  }
+
+  /** Gets the ParameterInput's index. */
+  get position() {
+    return this.index;
+  }
+
+  /** Gets the parent field of the ParamterInput (Tags, Ingredients, or Steps). */
+  get field() {
+    return this.parent;
+  }
+
+  /** Sets the text in a ParameterInput's text area. */
+  set text(value) {
+    this.textArea.value = value;
+  }
+
+  /** Sets the ParameterInput's index. */
+  set position(value)  {
+    this.index = parseInt(value);
+  }
+
+  /** Sets the parent field of the ParamterInput (Tags, Ingredients, or Steps). */
+  set field(value) {
+    this.parent = value;
+  }
+}
+customElements.define('parameter-input', ParameterInput);
+
+/** 
+ * Creates a ParameterInput.
+ * @param {string} name Used for displaying and determining the parent field.
+ * @param {string} index The position of the ParameterInput.
+ */
+function createParameterInput(name, index) {
+  var newParameter = document.createElement('parameter-input');
+  newParameter.setAttribute('name', name);
+  newParameter.setAttribute('index', index);
+  newParameter.setAttribute('id', name + index);
+  return newParameter;
+}
+
+/** 
+ * Inserts a ParameterInput.
+ * @param {ParameterInput} previous The ParameterInput to insert after.
+ * @param {ParameterInput} parameterInput The ParameterInput to insert.
+ */
+function insertParameterInput(previous, parameterInput) {
+  previous.insertAdjacentElement('afterend', parameterInput);
+  updateIndeces(parameterInput.field, parameterInput.position + 1);
+}
+
+/** 
+ * Appends an existing ParameterInput.
+ * @param {string} fieldName The field to append to.
+ * @param {ParameterInput} parameterInput The ParameterInput to append.
+ */
+function appendParameterInput(fieldName, parameterInput) {
+  const field = document.getElementById(fieldName);
+  field.appendChild(parameterInput);
+}
+
+/** 
+ * Appends a new ParameterInput.
+ * @param {string} fieldName The field to append to.
+ */
+function appendNewParameterInput(fieldName) {
+  const field = document.getElementById(fieldName);
+  field.appendChild(createParameterInput(fieldName.slice(0, -1), field.children.length));
+}
+
+/**
+ * Updates the indeces of ParameterInputs after an insertion or deletion.
+ * @param {string} fieldName The field with the ParameterInputs to update.
+ * @param {number} startIndex The index of the first ParameterInput to update.
+ */
+function updateIndeces(fieldName, startIndex) {
+  var parameters = document.getElementById(fieldName).children;
+  for (var i = startIndex; i < parameters.length; i++) {
+    parameters[i].position = i;
+    parameters[i].setIndexAttributes();
+  }
+}
+
+/** Gets a parent recipe's data from Datastore. */
+function getOriginalRecipe() {
+  const key = document.getElementById('key').value;
+  if (key !== '') {
+    fetch('/new-recipe?key=' + key).then(response => response.json()).then((recipe) => {
+      populateRecipeCreationForm(recipe);
+    });
+  }
+}
+
+/** Populates the fields of the recipe editor with a parent recipe's data. */
+function populateRecipeCreationForm(recipe) {
+  var name = document.getElementById('name');
+  name.value = recipe.name;
+
+  var description = document.getElementById('description');
+  description.value = recipe.description;
+
+  populateFormField('Tag', recipe.tags);
+  populateFormField('Ingredient', recipe.ingredients);
+  populateFormField('Step', recipe.steps);
+}
+
+/** Populates the ParamterInputs in a field with a parent recipe's data. */
+function populateFormField(fieldName, data) {
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    var parameter = document.getElementById(fieldName + i);
+    if (parameter !== null) {
+      parameter.text = getText(data[i]);
+    } else {
+      var newParameter = createParameterInput(fieldName, i);
+      newParameter.text = getText(data[i]);
+      appendParameterInput(fieldName + 's', newParameter);
+    }
+  }
+}
+
+/** Gets the text of a tag, ingredient, or step. */
+function getText(data) {
+  if (typeof data == 'string') {
+    return data;
+  } else {
+    return data.instruction;
+  }
+}
