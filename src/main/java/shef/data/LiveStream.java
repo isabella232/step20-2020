a4d13fb768e986a9177db333e7457146ecef5899
Angela <cheoalee@gmail.com>
2020-07-08 16:22:35 +0000

    Created basic livestream class.

---
+++ src/main/java/shef/data/LiveStream.java
@@ -0,0 +1,35 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package shef.data;
+ 
+/** A YouTube live stream.*/
+public class LiveStream {
+  long userId;
+  long recipeKey;
+  String link;
+  Datetime startTime;
+  Datetime endTime;
+
+ /**
+  * @param userId Unique id of the associated user (poster).
+  * @param recipeKey Unique key of the associated recipe.
+  * @param link Link to the live stream.
+  */
+  public UserComment(long userId, long recipeKey, String link) {
+      this.userId = userId;
+      this.recipeKey = recipeKey;
+      this.link = link;
+  }
+}   
