b81098b81461508fa869757bf068ec07da3c9bd3
Ryan Patrick <rmoneymail5@gmail.com>
2020-07-10 17:04:26 +0000

    Updated file path.

--- src/main/java/shef/servlets/ProfilePicUploadUrlServlet.java
+++ src/main/java/shef/servlets/ProfilePicUploadUrlServlet.java
@@ -12,35 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.sps.servlets;
+package shef.servlets;
 
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns a sign-in or sign-out URL. */
-@WebServlet("/sign-in")
-public class SignInServlet extends HttpServlet {
+/** Servlet that returns the Blobstore upload url for profile pictures. */
+@WebServlet("/profile-pic-upload-url")
+public class ProfilePicUploadUrlServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    String uploadUrl = blobstoreService.createUploadUrl("/user");
 
-    UserService userService = UserServiceFactory.getUserService();
-    String json;
-
-    if (userService.isUserLoggedIn()) {
-      String signOutUrl = userService.createLogoutURL("/sign-in-test.html");
-      json = "{ \"status\": true, \"url\": \"" + signOutUrl + "\" }";
-    } else {
-      String signInUrl = userService.createLoginURL("/user-list-test.html");
-      json = "{ \"status\": false, \"url\": \"" + signInUrl + "\" }";
-    }
-
-    response.setContentType("application/json");
-    response.getWriter().println(json);
+    response.setContentType("text/html");
+    response.getWriter().println(uploadUrl);
   }
 }
--- src/main/java/shef/servlets/UserListServlet.java
+++ src/main/java/shef/servlets/UserListServlet.java
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.sps.servlets;
+package shef.servlets;
 
-import com.google.sps.data.User;
+import shef.data.User;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
