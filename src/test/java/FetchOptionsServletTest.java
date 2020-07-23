d22214c19d6a64bfd64f28d114ee4d313f98d11b
Angela <cheoalee@gmail.com>
2020-07-23 18:05:41 +0000

    Fixed Set conversion errors in tests.

--- src/test/java/FetchOptionsServletTest.java
+++ src/test/java/FetchOptionsServletTest.java
@@ -6,8 +6,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RunWith(JUnit4.class)
 public class FetchOptionsServletTest {
@@ -15,48 +18,40 @@ public class FetchOptionsServletTest {
   /** Ensures that the substring splitting works for just one letter. */
   @Test
   public void titleCaseItemsTest_oneLetter() {
-    ArrayList<String> expectedList = new ArrayList<String>();
-    expectedList.add("E");
+    Set<String> expectedList = new HashSet<>(Arrays.asList("E")); 
 
-    ArrayList<String> testList = new ArrayList<String>();
-    testList.add("e");
-    ArrayList<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
+    Set<String> testList = new HashSet<>(Arrays.asList("e")); 
+    Set<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
 
     Assert.assertEquals(expectedList, actualList);
   }
 
   @Test
   public void titleCaseItemsTest_allCaps() {
-    ArrayList<String> expectedList = new ArrayList<String>();
-    expectedList.add("Egg");
+    Set<String> expectedList = new HashSet<>(Arrays.asList("Egg"));
 
-    ArrayList<String> testList = new ArrayList<String>();
-    testList.add("EGG");
-    ArrayList<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
+    Set<String> testList = new HashSet<>(Arrays.asList("EGG"));
+    Set<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
 
     Assert.assertEquals(expectedList, actualList);
   }
 
   @Test
   public void titleCaseItemsTest_noCaps() {
-    ArrayList<String> expectedList = new ArrayList<String>();
-    expectedList.add("Egg");
+    Set<String> expectedList = new HashSet<>(Arrays.asList("Egg"));
 
-    ArrayList<String> testList = new ArrayList<String>();
-    testList.add("egg");
-    ArrayList<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
+    Set<String> testList = new HashSet<>(Arrays.asList("egg"));
+    Set<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
 
     Assert.assertEquals(expectedList, actualList);
   }
 
   @Test
   public void titleCaseItemsTest_erraticCaps() {
-    ArrayList<String> expectedList = new ArrayList<String>();
-    expectedList.add("Bread");
+    Set<String> expectedList = new HashSet<>(Arrays.asList("Bread"));
 
-    ArrayList<String> testList = new ArrayList<String>();
-    testList.add("bReAD");
-    ArrayList<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
+    Set<String> testList = new HashSet<>(Arrays.asList("bReAD"));
+    Set<String> actualList = FetchOptionsServlet.titleCaseItems(testList);
 
     Assert.assertEquals(expectedList, actualList);
   }
